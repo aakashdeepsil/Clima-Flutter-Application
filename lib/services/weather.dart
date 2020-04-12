2ef8db55fe23c41d113a48adaf60b8be7dcb73bf
Aakashdeep Sil <aakashdeep.cs17@bmsce.ac.in>
2020-04-12 18:19:35 +0530

    First Commit

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,33 @@
+import 'location.dart';
+import 'networking.dart';
+
+const apiKey = 'b0c3843121947b946d2f5dfe834bb062';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    var URL = '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url: URL);
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        url:
+            '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
